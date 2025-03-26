0a6d366024ca6e0e1b96ff0d63ef7e6eb9886d9f
ISABELLY DOS SANTOS MORAES <CG3027112@car.ifsp>
2025-03-26 10:13:27 -0300

    Iniciando CNE

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React from 'react';
 import './Home.css';
+import Banner from './img/banner.png';
 
 const Home = () => {
     return (
-        <div className="home">
-            <h1></h1>
-        </div>
+        <section className="banner">
+            <div className="banner-img">
+                <img src={Banner} />
+            </div>
+            <div className="space">
+                <div className='line'></div>
+                <p className="title-space">Conheça nosso espaço</p>
+            </div>
+        </section>
     );
-}
+};
 
 export default Home;

9410839e13da165246ac4094636b895cdeb161b9
ISABELLY DOS SANTOS MORAES <CG3027112@car.ifsp>
2025-04-03 08:47:25 -0300

    tentando fazer Conheça nosso espaço

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -4,13 +4,31 @@ import Banner from './img/banner.png';
 
 const Home = () => {
     return (
-        <section className="banner">
+        <section className="our-space">
             <div className="banner-img">
                 <img src={Banner} />
             </div>
             <div className="space">
-                <div className='line'></div>
-                <p className="title-space">Conheça nosso espaço</p>
+                <div className="space-text">
+                    <div className="line"></div>
+                    <p className="title-space">Conheça nosso espaço</p>
+                    <p className="text-space">
+                        Cada detalhe foi pensado para proporcionar a você uma
+                        experiência inesquecível. Nosso espaço combina conforto,
+                        sofisticação e aconchego, criando o ambiente perfeito
+                        para desfrutar das melhores pizzas da cidade. Seja para
+                        um jantar romântico, uma reunião com amigos ou um
+                        momento especial em família, temos o espaço ideal para
+                        você!
+                    </p>
+                    <div className="line2"></div>
+                    <div className="text-space2">
+                        <p className="text1">
+                            Av. Bahia, 1739 - Indaiá, Caraguatatuba - SP
+                        </p>
+                        <p className="text2">De Terça a Domingo – 18h às 23h</p>
+                    </div>
+                </div>
             </div>
         </section>
     );

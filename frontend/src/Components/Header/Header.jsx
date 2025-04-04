3cb8ee8add76e95b827d704b27c72bec22a2e364
Gabriela Teixeira <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-04-04 11:56:00 -0300

    finalizando Header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -3,51 +3,79 @@ import './Header.css';
 import logo from './img/logo.png';
 
 const Header = () => {
+    const [showPopup, setShowPopup] = useState(false);
 
-  return (
-    <header id="Header">
-      <div id="LogoName">
-        <img src={logo} alt="Logo" />
-        <p>Vila Imperiale</p>
-      </div>
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+        if (showPopup === false) {
+            document.getElementById('openPopup').classList.add('openPopup');
+        } else {
+            document.getElementById('openPopup').classList.remove('openPopup');
+        }
+    };
 
-      <nav id="NavHeader">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Cardápio</a>
-          </li>
-          <li>
-            <a href="#">Pedido</a>
-          </li>
-          <li>
-            <a href="#">Reserva</a>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-          </li>
-        </ul>
+    return (
+        <header id="Header">
+            <div id="LogoName">
+                <img src={logo} alt="Logo"/>
+                <p>Vila Imperiale</p>
+            </div>
 
-        <div id="profile" onClick={PopupProfile}>
-          <p>^</p>
-          <ion-icon name="person-circle-outline"></ion-icon>
-        </div>
-      </nav>
-    </header>
-  );
+            <nav id="NavHeader">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Cardápio</a>
+                    </li>
+                    <li>
+                        <a href="#">Pedido</a>
+                    </li>
+                    <li>
+                        <a href="#">Reserva</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+
+                <div id="profile">
+                    <p
+                        id="openPopup"
+                        className="defaultPopup"
+                        onClick={togglePopup}
+                    >
+                        ^
+                    </p>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                </div>
+            </nav>
+
+            {showPopup && <PopupProfile />}
+        </header>
+    );
 };
 
 const PopupProfile = () => {
-  return (
-      <div id="popupProfile">
-          <p>Login/Logout</p>
-          <p>Pedido</p>
-          <p>Perfil</p>
-      </div>
-  );
-}
-
+    return (
+        <div id="popupProfile">
+            <div id="popupTriangle"></div>
+            <div id="popupText">
+                <ul>
+                    <li>
+                        <a>Login/Logout</a>
+                    </li>
+                    <li>
+                        <a>Pedido</a>
+                    </li>
+                    <li>
+                        <a>Perfil</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 export default Header;

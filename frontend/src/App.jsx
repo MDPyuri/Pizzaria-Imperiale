fba3384297e554c487280f04ed3a802c123e635e
Gabriela Teixeira <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-04-08 10:17:29 -0300

    iniciando rodapé

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header.jsx'
+import Footer from './Components/Footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
     <Header />
+    <Footer />
     </>
   )
 }

00668bf8106819cf29708c2887b5e328999c56cc
MDPyuri <matsumoto.prado@aluno.ifsp.edu.br>
2025-04-25 11:41:55 -0300

    atribuindo rotas para tabela Reservas

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+
+// Middleware para processar JSON
 app.use(express.json());
 
 // Importar rotas de usuário
 const usuarioRoutes = require("./Routes/usuario");
 app.use("/usuarios", usuarioRoutes);
+
+// Importar rotas de reserva
+const reservaRoutes = require("./Routes/reserva");
+app.use("/reservas", reservaRoutes);
 
 // Iniciar o servidor
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));

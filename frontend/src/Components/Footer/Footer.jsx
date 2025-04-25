df5aac73a5bd4f5a54720903e78d388d69fc4f00
Gabriela Teixeira <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-04-25 11:48:33 -0300

    finalizando rodapé

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -4,51 +4,54 @@ import '../Footer/Footer.css';
 const Footer = () => {
     return (
         <footer id="Footer">
-            <div id="title-rod">
-                <div className="line-rod"></div>
-                <p>Fale conosco</p>
-                <div className="line-rod"></div>
-            </div>
-
-            <div id="content-rod">
-                <div id="text-rod">
-                    <p>
-                        Tem alguma dúvida, quer saber mais sobre o Vila
-                        Imperiale ou dar um feedback?
-                    </p>
-                    <p>
-                        Envie sua mensagem pelas nossas redes sociais ou pelo
-                        nosso whatsapp e retornaremos o mais breve possível!
-                    </p>
+            <div id="contentRod">
+                <div id="title-rod">
+                    <div className="line-rod"></div>
+                    <p>Fale conosco</p>
+                    <div className="line-rod"></div>
                 </div>
 
-                <div id="socialmedia-rod">
-                    <div className="item-socialmedia">
-                        <ion-icon name="logo-whatsapp"></ion-icon>
-                        <p>(12) 3881-2540</p>
+                <div id="content-rod">
+                    <div id="text-rod">
+                        <p>
+                            Tem alguma dúvida, quer saber mais sobre o Vila
+                            Imperiale ou dar um feedback?
+                        </p>
+                        <p>
+                            Envie sua mensagem pelas nossas redes sociais ou
+                            pelo nosso whatsapp e retornaremos o mais breve
+                            possível!
+                        </p>
                     </div>
-                    <div className="item-socialmedia">
-                        <ion-icon name="logo-instagram"></ion-icon>
-                        <p>@vilaimperiale_</p>
-                    </div>
-                    <div className="item-socialmedia">
-                        <ion-icon name="logo-tiktok"></ion-icon>
-                        <p>@vilaimperiale_</p>
-                    </div>
-                    <div className="item-socialmedia">
-                        <ion-icon name="logo-facebook"></ion-icon>
-                        <p>VilaImperiale</p>
+
+                    <div id="socialmedia-rod">
+                        <div className="item-socialmedia">
+                            <ion-icon name="logo-whatsapp"></ion-icon>
+                            <p>(12) 3881-2540</p>
+                        </div>
+                        <div className="item-socialmedia">
+                            <ion-icon name="logo-instagram"></ion-icon>
+                            <p>@vilaimperiale_</p>
+                        </div>
+                        <div className="item-socialmedia">
+                            <ion-icon name="logo-tiktok"></ion-icon>
+                            <p>@vilaimperiale_</p>
+                        </div>
+                        <div className="item-socialmedia">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                            <p>VilaImperiale</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="line-rod2"></div>
+                <div className="line-rod2"></div>
 
-            <div id="copyright-rod">
-                <p>
-                    Vila Imperiale © 2025 - Todos os direitos reservados / todas
-                    as imagens deste site foram criadas por IA
-                </p>
+                <div id="copyright-rod">
+                    <p>
+                        Vila Imperiale © 2025 - Todos os direitos reservados /
+                        todas as imagens deste site foram criadas por IA
+                    </p>
+                </div>
             </div>
         </footer>
     );

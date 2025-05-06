162ec46df03976513a84ab57ab450ee1fa608e6a
Milly02online <Milly02online@gmail.com>
2025-05-05 21:21:50 -0300

    Iniciando primeira div product para pizzas salgadas

--- frontend/src/Components/Menu/Menu.jsx
+++ frontend/src/Components/Menu/Menu.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Menu.css';
 
+import pizzaSal1 from '../../assets/img/pizzaSal1.png';
+
 const Menu = () => {
     return (
         <>
@@ -28,6 +30,39 @@ const Menu = () => {
                 <button><ion-icon name="search-outline"></ion-icon></button>
             </div>
             <div className="line-two"></div>
+
+            <div className="containerCentral">
+                <div className="product">
+                    <div className="img">
+                       <img src={pizzaSal1} alt="Pizza Salgada"/>
+                       <p>const</p>
+                    </div>
+                    <div className="description">
+                       <p>Tartufo Nero e Burrata</p>
+                       <div className="descButton">
+                            <p className="pDesc">Descrição...</p>
+                            {/* botao de descer */}
+                            <button className="buttonDesc">a</button>
+                       </div>
+                       
+                       <p className="pBroto">Broto - R$95,90</p>
+                       <div className="counter">
+                            <button> - </button>
+                            <button> 0 </button>
+                            <button> + </button>
+                       </div>
+                       <p className="pPizza">Pizza - R$189,90</p>
+                       <div className="counter">
+                            <button> - </button>
+                            <button> 0 </button>
+                            <button> + </button>
+                       </div>
+                    </div>
+
+                    <button className="addCar">Adicionar ao meu carrinho</button>
+                    
+                </div>
+            </div>
         </>
     )
 }

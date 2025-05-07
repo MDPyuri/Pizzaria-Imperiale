52b44696d5fcc3a853f1845099d24c0fa442130a
Milly02online <Milly02online@gmail.com>
2025-05-06 21:35:43 -0300

    Terminando a primeira div pizza salgada: sistema de contagem e sistema de substituição de texto finalizados. falta sistema de avaliacao

--- frontend/src/Components/Menu/Menu.jsx
+++ frontend/src/Components/Menu/Menu.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './Menu.css';
+import ReactStars from 'react-rating-stars-component';
 
+// fotos das pizzas salgadas
 import pizzaSal1 from '../../assets/img/pizzaSal1.png';
 
 const Menu = () => {
+
+    //sistema de contagem para pizzas e brotos salgados por id
+    const [brotoSalg1, setBrotoSalg1] = useState(0);
+    const [pizzaSalg1, setPizzaSalg1] = useState(0);
+
+    //sistema de substituir descrição
+    const [showFullDesc, setShowFullDesc] = useState(false);
+
+
     return (
         <>
             <div className="menu">
@@ -35,28 +46,72 @@ const Menu = () => {
                 <div className="product">
                     <div className="img">
                        <img src={pizzaSal1} alt="Pizza Salgada"/>
-                       <p>const</p>
+                       {/* sistema de avaliação */}
+                       <ReactStars
+                            count={5}
+                            onChange={(newRating) => console.log(`Avaliação: ${newRating}`)}
+                            size={50}
+                            isHalf={true}
+                            activeColor="#c5a15c"
+                            className="react-stars"
+                            style={{ width: '50px', height: '50px' }}
+                        />
                     </div>
                     <div className="description">
                        <p>Tartufo Nero e Burrata</p>
                        <div className="descButton">
-                            <p className="pDesc">Descrição...</p>
-                            {/* botao de descer */}
-                            <button className="buttonDesc">a</button>
+                            {!showFullDesc && <p className="pDesc">Descrição...</p>}
+                            {showFullDesc && (
+                                <p className="pDesc2">
+                                    Massa de fermentação lenta, molho bechamel trufado, burrata fresca, cogumelos selvagens, azeite de trufas negras e lascas de parmesão 36 meses.
+                                </p>
+                            )}
+                            {!showFullDesc && (
+                                <button className="buttonDesc" onClick={() => setShowFullDesc(true)}>
+                                    <ion-icon name="chevron-down-circle-outline"></ion-icon>
+                                </button>
+                            )}
+                            {showFullDesc && (
+                                <button className="buttonDesc2" onClick={() => setShowFullDesc(false)}>
+                                    <ion-icon name="chevron-up-circle-outline"></ion-icon>
+                                </button>
+                            )}
+
                        </div>
                        
-                       <p className="pBroto">Broto - R$95,90</p>
-                       <div className="counter">
-                            <button> - </button>
-                            <button> 0 </button>
-                            <button> + </button>
-                       </div>
-                       <p className="pPizza">Pizza - R$189,90</p>
-                       <div className="counter">
-                            <button> - </button>
-                            <button> 0 </button>
-                            <button> + </button>
-                       </div>
+                       <div className={`counter-wrapper ${showFullDesc ? "ajuste-margin" : ""}`}>
+                            <p className="pBroto">Broto</p>
+                            <p className="pBrotoDivisao">-</p>
+                            <p className="pBrotoPreco">R$95,90</p>
+                            <div className="counter">
+                                    <button 
+                                        id="-brotoSalg1" 
+                                        onClick={() => setBrotoSalg1(prev => prev > 0 ? prev - 1 : 0)}> 
+                                    - </button>
+                                    <button id="brotoSalgCounter1"> {brotoSalg1} </button>
+                                    <button 
+                                        id="+brotoSalg1" 
+                                        onClick={() => setBrotoSalg1(prev => prev + 1)}> 
+                                    + </button>
+                            </div>
+                            
+                        </div>
+                        <div className="counter-wrapper2">
+                            <p className="pPizza">Pizza</p> 
+                            <p className="pPizzaDivisao"> - </p>
+                            <p className="pPizzaPreco">R$189,90</p>
+                            <div className="counter">
+                                    <button 
+                                        id="-pizzaSalg1" 
+                                        onClick={() => setPizzaSalg1(prev => prev > 0 ? prev - 1 : 0)}> 
+                                    - </button>
+                                    <button id="pizzaSalgCounter1"> {pizzaSalg1} </button>
+                                    <button 
+                                        id="+pizzaSalg1" 
+                                        onClick={() => setPizzaSalg1(prev => prev + 1)}> 
+                                    + </button>
+                            </div>
+                        </div>
                     </div>
 
                     <button className="addCar">Adicionar ao meu carrinho</button>

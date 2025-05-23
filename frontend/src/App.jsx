0c871810dbcedb2c19b0278e7c095e88b003b5fa
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-05-23 10:56:41 -0300

    consertando erro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/Home/Home'
 import Header from './Components/Header/Header.jsx'
 import Footer from './Components/Footer/Footer.jsx'
 import ReservationPage from './Components/ReservationPage/ReservationPage.jsx'
+import Reservation from './Components/Reservation/Reservation.jsx'
 
 function App() {
 
@@ -13,7 +14,6 @@ function App() {
     <Home/>
     <Reservation/>
     <Footer/>
-    <ReservationPage/>
     </>
   )
 }

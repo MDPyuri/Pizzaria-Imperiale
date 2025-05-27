6355e6cb7520bccc1d8da29f27b7530be8e2cb9a
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-05-27 10:19:29 -0300

    arrumando App e deletando about (por enquanto)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import ReservationPage from './Components/ReservationPage/ReservationPage.jsx';
 import Reservation from './Components/Reservation/Reservation.jsx';
+import AboutUs from './Components/AboutUs/AboutUs.jsx';
 
 function App() {
     return (
@@ -16,6 +18,7 @@ function App() {
                         <>
                             <Home />
                             <Reservation />
+                            <AboutUs />
                             <Footer />
                         </>
                     }

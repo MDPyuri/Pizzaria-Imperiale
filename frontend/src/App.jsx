f938a6cc71d08d84d2e4a36238761b63a05ebd34
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-05-27 10:24:55 -0300

    atualizando DE NOVO a main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
@@ -6,16 +7,24 @@ import ReservationPage from './Components/ReservationPage/ReservationPage.jsx';
 import Reservation from './Components/Reservation/Reservation.jsx';
 
 function App() {
-
-  return (
-    <>
-    <Header/>
-    {/* <Home/> */}
-    {/* <Footer/> */}
-    <ReservationPage/>
-    </>
-  )
-    
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <Home />
+                            <Reservation />
+                            <Footer />
+                        </>
+                    }
+                />
+                <Route path="/reserva" element={<ReservationPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;

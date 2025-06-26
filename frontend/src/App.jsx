3f8adb0e2a20e07553f1422ef1bc8318bd6bbc96
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-06-26 09:55:50 -0300

    Apresentação do cardapio na home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react'
-import './App.css'
-import Home from './Components/Home/Home'
-import Header from './Components/Header/Header.jsx'
-import Footer from './Components/Footer/Footer.jsx'
-import ReservationPage from './Components/ReservationPage/ReservationPage.jsx'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import ReservationPage from './Components/ReservationPage/ReservationPage.jsx';
 import Reservation from './Components/Reservation/Reservation.jsx';
-import Cart from './Components/Order/Cart.jsx';
-import Address from './Components/Order/Address.jsx';
-import Payment from './Components/Order/Payment.jsx';
+// import Cart from './Components/Order/Cart.jsx';
+// import Address from './Components/Order/Address.jsx';
+// import Payment from './Components/Order/Payment.jsx';
 import UserLogin from './Components/CRUD/Login.jsx'
 import CreateUser from './Components/CRUD/Create.jsx'
 import UpdateUser from './Components/CRUD/Update.jsx'
 import UserProfile from './Components/CRUD/Profile.jsx'
 import DeleteUser from './Components/CRUD/Delete.jsx'
-import FilterPizza from './Components/Menu/FilterPizza.jsx';
-import ProductList from './Components/Menu/ProductList.jsx';
 import Menu from './Components/Menu/Menu.jsx';
 import About from './Components/About/About.jsx';
-import Reservation from './Components/Reservation/Reservation.jsx'
+import CallToMenu from './Components/CallToMenu/CallToMenu.jsx';
 
 // Simulação de autenticação (substitua por lógica real)
 // const isAuthenticated = () => {
@@ -47,15 +38,18 @@ const isAuthenticated = async () => {
 };
 
 
-// function MainApp() {
-//   return (
-//     <>
-//       <Header />
-//       <Home />
-//       <Footer />
-//     </>
-//   );
-// }
+function MainApp() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <CallToMenu />
+      <Reservation />
+      <About />
+      <Footer />
+    </>
+  );
+}
 
 // function App() {
 //   return (
@@ -89,33 +83,33 @@ const isAuthenticated = async () => {
 //   );
 // }
 
-function App() {
-    return (
-        <>
-            <Router>
-                <Header />
-                {/* <Menu/> */}
-                <Routes>
-                    <Route path="/carrinho" element={<Cart />} />
-                    <Route path="/endereco" element={<Address />} />
-                    <Route path="/pagamento" element={<Payment />} />
-                </Routes>
-            </Router>
-        </>
-    );
-  return (
-    <>
-      <Menu/>
-    </>
-  )
-      <Header />
-      <Home />
-      <Reservation />
-      <About />
-      <Footer />
-    </>
-  );
-}
+// function App() {
+//     return (
+//         <>
+//             <Router>
+//                 <Header />
+//                 {/* <Menu/> */}
+//                 <Routes>
+//                     <Route path="/carrinho" element={<Cart />} />
+//                     <Route path="/endereco" element={<Address />} />
+//                     <Route path="/pagamento" element={<Payment />} />
+//                 </Routes>
+//             </Router>
+//         </>
+//     );
+//   return (
+//     <>
+//       <Menu/>
+//     </>
+//   )
+//       <Header />
+//       <Home />
+//       <Reservation />
+//       <About />
+//       <Footer />
+//     </>
+//   );
+// }
 
 function App() {
   return (
--- frontend/src/Components/CallToMenu/CallToMenu.jsx
+++ frontend/src/Components/CallToMenu/CallToMenu.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./CallToMenu.css";
-import pizza1 from "./assets/1.png";
-import pizza2 from "./assets/2.png";
+import { Link } from 'react-router-dom';
 
 const CallToMenu = () => {
     return (
@@ -12,52 +11,15 @@ const CallToMenu = () => {
                     <div className="title-call">Cardápio</div>
                     <div className="line-call"></div>
                 </div>
-                <div className="boxes-call">
-                    <div className="box">
-                        <div className="photo-call">
-                            <img
-                                className="photo"
-                                src={pizza1}
-                                alt="Tartufo Nero e Burrata"
-                            />
-                        </div>
-                        <div className="text-content">
-                            <div className="pizza-title">
-                                <p>Tartufo Nero e Burrata</p>
-                            </div>
-                            <div className="pizza-text">
-                                <p>
-                                    Massa de fermentação lenta, molho bechamel
-                                    trufado, burrata fresca, cogumelos
-                                    selvagens, azeite de trufas negras e lascas
-                                    de parmesão 36 meses.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="box">
-                        <div className="photo-call">
-                            <img
-                                className="photo"
-                                src={pizza2}
-                                alt="Filetto e Gorgonzola"
-                            />
-                        </div>
-                        <div className="text-content">
-                            <div className="pizza-title">
-                                <p>Filetto e Gorgonzola</p>
-                            </div>
-                            <div className="pizza-text">
-                                <p>
-                                    Molho pomodoro italiano, filé mignon selado
-                                    em manteiga de garrafa, gorgonzola dolce,
-                                    cebola caramelizada e nozes tostadas.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="btn-content-call">
+                    <Link to="/menu">
+                        <button className="btn-call">
+                            {' '}
+                            ACESSE O CARDÁPIO{' '}
+                        </button>
+                    </Link>
                 </div>
-                <div className="line2-call"></div>
+                <div className="line-call"></div>
             </div>
         </section>
     );

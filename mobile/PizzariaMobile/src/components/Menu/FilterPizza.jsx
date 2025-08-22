6abf2d39f5028848c4743f60dd9404ea1e4c7a0b
milly02online <milly02online@gmail.com>
2025-08-22 12:03:45 -0300

    Criando o FilterPizza

---
+++ mobile/PizzariaMobile/src/components/Menu/FilterPizza.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, Button, Image, StyleSheet } from "react-native";
+
+export default function FilterPizza() {
+    return (
+        <View>
+            <Text> Olá</Text>
+        </View>
+    )
+}
---
+++ mobile/PizzariaMobile/src/screens/MenuScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, Button, Image, View, StyleSheet } from "react-native";
+
+import FilterPizza from "../components/Menu/FilterPizza";
+
+export default function MenuScreen() {
+    return (
+        <View>
+            <Text> Olá</Text>
+        </View>
+    )
+}

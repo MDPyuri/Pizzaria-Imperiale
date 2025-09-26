04e751d6e30d5cacd92a10b177f855a715b23467
Gabriela <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-09-26 12:04:19 -0300

    Estrutura da página de endereço

--- mobile/PizzariaMobile/src/components/NavBar.jsx
+++ mobile/PizzariaMobile/src/components/NavBar.jsx
@@ -17,7 +17,7 @@ export default function NavBar({ navigation }) {
                 <FontAwesome5 name="pizza-slice" size={30} color="#f5f3e7" />
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Screen3')}>
+            <TouchableOpacity onPress={() => navigation.navigate('AddressScreen')}>
                 <FontAwesome5 name="shopping-cart" size={30} color="#f5f3e7" />
             </TouchableOpacity>
 
